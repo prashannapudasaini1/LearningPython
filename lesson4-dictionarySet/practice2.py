5d67cbed40df383f75aaf7bf15f02c424c40662b
Prashanna Pudasaini <139542814+prashannapudasaini1@users.noreply.github.com>
2025-05-12 19:08:12 +0000

    added by Prashanna

---
+++ lesson4-dictionarySet/practice2.py
@@ -0,0 +1,8 @@
+#  you are given a list of subjects of student.
+# assume one calssroom is required for 1 subject.
+# How many classroom are needed by all students?
+# "python", "java", "c++", "python""javascript", "java", "python", "java", "c++", "c"
+
+subject = {"python", "java", "c++", "python"
+           "javascript", "java", "python", "java", "c++", "c"}
+print("The number of classroom needed is:", len(subject))  # Output: 5
