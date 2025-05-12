b108f6f6013ba5aa318bbbc87d680a517a948245
Prashanna Pudasaini <139542814+prashannapudasaini1@users.noreply.github.com>
2025-05-12 18:53:14 +0000

    added by prashanna

---
+++ lesson4-dictionarySet/practice1.py
@@ -0,0 +1,13 @@
+#  Store following data in a dictionary:
+# table: "a piece of furniture", "list of facts & figures"
+# Cat : "a small animal"
+
+store = {
+    "table": {
+        "a piece of furniture",
+        "list of facts & figures"
+        },
+    "cat" : "a small animal",
+}
+
+print(store)
